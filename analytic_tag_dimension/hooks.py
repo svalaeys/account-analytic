23792d26a3a1671f6019b2e9d7cbd6b98490e45c
Stéphane Valaeys <svalaeys@fiefmanage.ch>
2025-01-09 18:11:38 +0100

    Initial commit

---
+++ analytic_tag_dimension/hooks.py
@@ -0,0 +1,8 @@
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import SUPERUSER_ID, api
+
+
+def uninstall_hook(env):
+    """Cleanup all dimensions before uninstalling."""
+    env["account.analytic.dimension"].search([]).unlink()
